e0e8a77319f654d5514e632f040f8868378e275b
Lauren Fernandez <lauren@alley.co>
2021-02-08 12:37:11 -0800

    shows code at end of lesson 8 routing to controllers

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
-class PostsController
+use Illuminate\Http\Request;
 
+class PostsController extends Controller
 {
- public function show($post){
-  $posts = [
-    'my-first-post' => 'Hello, this is my first blog post. ',
-    'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-  ];
-
-  if (! array_key_exists($post, $posts)) {
-    abort(404, 'Sorry, that post was not found.');
+  public function show($post){
+    $posts = [
+      'my-first-post' => 'Hello, this is my first blog post. ',
+      'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+    
+    if (! array_key_exists($post, $posts)) {
+      abort(404, 'Sorry, that post was not found.');
+    }
+    
+    return view('post', [
+      'post' => $posts[$post]
+    ]);
   }
-
-  return view('post', [
-    'post' => $posts[$post]
-  ]);
- } 
 }
+
