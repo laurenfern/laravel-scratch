b5b1d37a8f466a0286463f573e533e9b05141127
Lauren Fernandez <lauren@alley.co>
2021-02-08 11:31:36 -0800

    add code comments

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// the {post} is a wildcard, it will accept anything. 
+// Ex) the URI http://laravel6tutorial.test/posts/asdfgh would work, and $post would be set to 'asdfgh'
 Route::get('/posts/{post}', function ($post) {
     $posts = [
         'my-first-post' => 'Hello, this is a first blog post.',
         'my-second-post' => 'Now I am getting the hang of this blogging thing.'
     ];
 
+    // check if the string from the URI (the $post) matches a key (ie slug) in the $posts array
     if (!array_key_exists($post, $posts)) {
         abort(404, 'Sorry, that post was not found.');
     }
  
     return view('post', [
+        // this is the data to be passed to the 'post' view at resources/views/post.blade.php
         'post' => $posts[$post]
     ]);
 });
