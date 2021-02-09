105ec007cf0e7ac7346a5024d6c87e2f425ede7f
Lauren Fernandez <lauren@alley.co>
2021-02-09 13:22:32 -0800

    captures end of lesson code

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,18 +6,21 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-  public function show($post){
-    $posts = [
-      'my-first-post' => 'Hello, this is my first blog post. ',
-      'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-    
-    if (! array_key_exists($post, $posts)) {
-      abort(404, 'Sorry, that post was not found.');
-    }
-    
+  public function show($slug)
+  {
+    // This query says "using the DB class, look in the table named 'posts', where the 'slug'column 
+    // is equal to the value we got from the URI (the $slug argument in the show function above), and 
+    // give me the first result.
+    $post = \DB::table('posts')->where('slug', $slug)->first();
+
+    // We're now fetching data from a database, so no need to hardcode the blog posts here.
+    // $posts = [
+    //   'my-first-post' => 'Hello, this is my first blog post. ',
+    //   'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    // ];
+
     return view('post', [
-      'post' => $posts[$post]
+      'post' => $post
     ]);
   }
 }
