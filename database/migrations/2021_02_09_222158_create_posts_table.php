5342a7e77d1db35df90925616470ca77011f0bd8
Lauren Fernandez <lauren@alley.co>
2021-02-09 16:00:55 -0800

    captures code at end of lesson

--- database/migrations/2021_02_09_222158_create_posts_table.php
+++ database/migrations/2021_02_09_222158_create_posts_table.php
@@ -14,8 +14,12 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); //included by default
+            $table->string('slug');
+            $table->text('body');
+            $table->string('title');
+            $table->timestamps(); //included by default. sets "created at" and "updated at" timestamps
+            $table->timestamp('published_at')->nullable();//allows post to publish at a later date. this field can be nullified, ie its optional.
         });
     }
 
