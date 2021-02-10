1b5a055174aa9b5014600f7a665f843f31f5754f
Lauren Fernandez <lauren@alley.co>
2021-02-10 15:28:00 -0800

    captures end of lesson code. creates Assignment model, controller and migration

---
+++ database/migrations/2021_02_10_205236_create_assignments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            // another option instead of boolean('completed') is timestamp('completed')->nullable()
+            // which would tell us when the item was completed AND would be nullable if not completed.
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+            $table->timestamp('due_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
