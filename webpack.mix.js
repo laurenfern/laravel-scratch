2881bd462ff6782ccaa4335c4b2366c70a5d2b64
Lauren Fernandez <lauren@alley.co>
2021-02-12 15:49:48 -0800

    end of lesson code. error with can't resolve /resources/sass/app.css in root

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ const mix = require('laravel-mix');
  */
 
 // mix.js('resources/js/app.js', 'public/js')
-    mix.sass('resources/sass/app.css', 'public/css');
+mix.sass('resources/sass/app.css', 'public/css');
