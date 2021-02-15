46f5eb054d7e5f0e496ac1f06a06eac9e8fc488a
Lauren Fernandez <lauren@alley.co>
2021-02-15 11:27:27 -0800

    comment on seven restful controller actions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,6 +9,7 @@ class ArticlesController extends Controller
 {
   public function index()
   {
+    // Render a list of a resource.
     $articles = Article::latest()->get();
 
     return view('articles.index', ['articles' => $articles]);
@@ -16,8 +17,34 @@ class ArticlesController extends Controller
 
   public function show($id)
     {
+      // Show a single resource
       $article = Article::find($id);
 
       return view('articles.show', ['article' => $article]);
     }
+
+  public function create()
+  {
+    // Shows a view to create a new resource
+  }
+
+  public function store()
+  {
+    // Persist the new resource (save it)
+  }
+  
+  public function edit()
+  {
+    // Show a view to edit an existing resource
+  }
+  
+  public function update()
+  {
+    // Persist the edited resource
+  }
+
+  public function destroy()
+  {
+    // Delete the resource
+  }
 }
