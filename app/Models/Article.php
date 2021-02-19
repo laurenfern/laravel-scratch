598747c254ae1f0efd50359f433762cde3cd1c8e
Lauren Fernandez <lauren@alley.co>
2021-02-19 12:46:29 -0800

    comment out example code for fetching article by slug instead of id

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,14 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
-
-  // overwrite the getRouteKeyName function, so that we can fetch an article by its slug instead of its id (its primary key)
-  public function getRouteKeyName()
-  {
-    return 'slug'; // this should return the name of the column that you want to query by
-    // behind the scenes, Laravel is doing the following
-    // Article::where('slug', $article)->first()
-    // "Give me the article where the value in the column 'slug' matches the input from the wildcard {$article}
-    // from routes/web.php Route::get('/articles/{article}' "
-  }  
+/* Example for fetching an article by its slug instead of its id (its primary key)
+    
+      // overwrite the getRouteKeyName function 
+              public function getRouteKeyName()
+            {
+              return 'slug'; 
+            }
+      // this should return the name of the column that you want to query by
+      // behind the scenes, Laravel is doing the following
+      // Article::where('slug', $article)->first()
+      // "Give me the article where the value in the column 'slug' matches the input from the wildcard {$article}
+      // from routes/web.php Route::get('/articles/{article}' "
+*/
 }
